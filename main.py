6cd4d2be66f8aaf79ea2b3ff5479e51bccf09ad4
nicafaynberg <berenis0102@gmail.com>
2021-05-31 23:55:54 +0300

    version 31 may

--- main.py
+++ main.py
@@ -49,11 +49,12 @@ CHOOSING, SETTING_THE_QUESTION, SETTING_TIME, DELETING_TASKS, SAYING_GOODBYE = r
 SET_TASK_TEXT = 'Добавить'
 DOWNLOAD_ANSWERS_TEXT = 'Скачать ответы'
 SHOW_MY_TASKS_TEXT = 'Мои вопросы'
+SHOW_ALL_DATA = "Показать все"
 DELETE_TASK = "Удалить вопрос"
 GOODBYE = "На этом все"
 
 reply_keyboard = [
-    [SET_TASK_TEXT, SHOW_MY_TASKS_TEXT, DOWNLOAD_ANSWERS_TEXT, DELETE_TASK, GOODBYE]
+    [SET_TASK_TEXT, SHOW_MY_TASKS_TEXT, DOWNLOAD_ANSWERS_TEXT, SHOW_ALL_DATA, DELETE_TASK, GOODBYE]
 ]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True)
 updater = None
@@ -90,32 +91,6 @@ def received_information_text(update: Update, context: CallbackContext) -> int:
     )
     return SETTING_TIME
 
-
-def set_answer(update: Update, context: CallbackContext) -> int:
-    update.message.reply_text("Спасибо за ответ!")
-    question_text = context.user_data['question_for_upcoming_answer']
-    answer_text = update.message.text
-    del context.user_data['question_for_upcoming_answer']
-
-    if 'answers' not in context.user_data:
-        context.user_data['answers'] = []
-
-    context.user_data['tasks'].append({
-        "date": datetime.datetime.now().isoformat(),
-        "question": question_text,
-        "answer": answer_text
-    })
-    update.message.reply_text(f"""
-    Вы ответили
-    {answer_text}  
-    на вопрос
-    {question_text}
-    Отлично!
-    До встречи!
-    """)
-    return CHOOSING
-
-
 def received_information_time(update: Update, context: CallbackContext) -> int:
     task_time = update.message.text
     if not re.match(r"^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$", task_time):
@@ -133,16 +108,6 @@ def received_information_time(update: Update, context: CallbackContext) -> int:
 
     context.user_data['tasks'].append(task)
 
-    def callback_minute(ctx):
-        context.user_data['question_for_upcoming_answer'] = task_text
-        context.bot.send_message(update.effective_user.id, text=task_text, reply_markup=ForceReply())
-
-    os.environ['TZ'] = 'Europe/Moscow'
-
-    # d = datetime.time(hour=0, minute=0, second=0, microsecond=0, tzinfo=None)
-    d = datetime.datetime.strptime(task_time, '%H:%M').time()
-    updater.job_queue.run_daily(callback_minute, time=d)
-    print(d)
     del context.user_data['text_for_upcoming_task']
 
     update.message.reply_text(
@@ -152,12 +117,45 @@ def received_information_time(update: Update, context: CallbackContext) -> int:
         reply_markup=markup,
     )
 
+    def callback_minute(ctx):
+        # context.user_data['question_for_upcoming_answer'] = task_text
+        # context.bot.send_message(update.effective_user.id, text=task_text, reply_markup=ForceReply())
+
+        os.environ['TZ'] = 'Europe/Moscow'
+
+    # d = datetime.time(hour=0, minute=0, second=0, microsecond=0, tzinfo=None)
+        d = datetime.datetime.strptime(task_time, '%H:%M').time()
+        updater.job_queue.run_daily(callback_minute, time=d)
+
     return CHOOSING
 
+def set_answer(update: Update, context: CallbackContext) -> int:
+    update.message.reply_text("Спасибо за ответ")
+    question_text = context.user_data['question_for_upcoming_answer']
+    answer_text = update.message.text
+    del context.user_data['question_for_upcoming_answer']
+
+    if 'answers' not in context.user_data:
+        context.user_data['answers'] = []
+
+    context.user_data['answers'].append({
+        "date": datetime.datetime.now().isoformat(),
+        "question": question_text,
+        "answer": answer_text
+    })
+    update.message.reply_text(f"""
+    Вы ответили
+    {answer_text}  
+    на вопрос
+    {question_text}
+    Отлично, до встречи!
+    """)
+
+    return CHOOSING
 
 def show_all_data(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(
-        f"Вот что вы уже мне рассказали: {facts_to_str(context.user_data)}"
+        f"Вот что вы уже мне рассказали: {context.user_data}"
     )
     return CHOOSING
 
@@ -181,14 +179,19 @@ def offer_to_delete(update: Update, context: CallbackContext) -> None:
     return DELETING_TASKS
 
 def delete_tasks(update: Update, context: CallbackContext) -> None:
-    task_list = context.user_data['tasks']
-    for i in task_list.index():
-        del context.user_data[i]
-    # del context.user_data['tasks']
+    if not update.message.text.isnumeric():
+        update.message.reply_text("Введите число (номер задачи)")
+        return DELETING_TASKS
+    idx = int(update.message.text)
+    if idx >= len(context.user_data['tasks']) or idx < 0:
+        update.message.reply_text("Неверное число")
+        return DELETING_TASKS
+
+    del context.user_data['tasks'][idx]
     update.message.reply_text("Удалил")
+    update.message.reply_text("Что-то еще?", reply_markup=markup)
 
     return CHOOSING
-
 
 def done(update: Update, context: CallbackContext) -> int:
     # if 'choice' in context.user_data:
@@ -210,6 +213,13 @@ def download_answers(update: Update, context: CallbackContext):
     fname = f'results_{user_id}.xlsx'
     workbook = xlsxwriter.Workbook(fname)
     worksheet = workbook.add_worksheet()
+
+    bold = workbook.add_format({'bold': True})
+    worksheet.write('A1', 'Дата и время ответа', bold)
+    worksheet.write('B1', 'Вопрос', bold)
+    worksheet.write('C1', 'Время задавания вопроса', bold)
+    worksheet.write('D1', 'Ответ', bold)
+
     for idx, task in enumerate(context.user_data['tasks']):
         row_index = idx + 1
         if 'date' in task:
@@ -241,11 +251,11 @@ def main() -> None:
         states={
             CHOOSING: [
                 MessageHandler(Filters.regex(f'^{SET_TASK_TEXT}$'), set_task_choice),
-                # MessageHandler(Filters.regex(f'^{SHOW_MY_TASKS_TEXT}$'), show_all_data),
+                MessageHandler(Filters.regex(f'^{SHOW_ALL_DATA}$'), show_all_data),
                 MessageHandler(Filters.regex(f'^{SHOW_MY_TASKS_TEXT}$'), show_tasks_only),
                 MessageHandler(Filters.regex(f'^{DOWNLOAD_ANSWERS_TEXT}$'), download_answers),
-                MessageHandler(Filters.regex(f'^{GOODBYE}$'), done),
                 MessageHandler(Filters.regex(f'^{DELETE_TASK}$'), offer_to_delete),
+                MessageHandler(Filters.regex(f'^{GOODBYE}$'), done),
             ],
             SETTING_THE_QUESTION: [
                 MessageHandler(
@@ -261,8 +271,13 @@ def main() -> None:
                                received_information_time,
                                )
             ],
+            # ANSWERING: [
+            #     MessageHandler(Filters.text,
+            #                    set_answer,
+            #                    )
+            # ],
             SAYING_GOODBYE: [
-                MessageHandler(Filters.text, ConversationHandler.END)
+                MessageHandler(Filters.text, done)
             ]
         },
         fallbacks=[MessageHandler(Filters.regex('^Done$'), done)],
@@ -275,16 +290,8 @@ def main() -> None:
     show_all_data_handler = CommandHandler('show_all_data', show_all_data)
     dispatcher.add_handler(show_all_data_handler)
 
-    # show_tasks_only_handler = CommandHandler('show_tasks_only', show_tasks_only)
-    # dispatcher.add_handler(show_tasks_only)
-
     # Start the Bot
     updater.start_polling()
-    # updater.start_webhook(listen="0.0.0.0",
-    #                       port=int(PORT),
-    #                       url_path=getenv('TELEGRAM_TOKEN'),
-    #                       webhook_url='https://hidden-lake-49896.herokuapp.com/' + getenv('TELEGRAM_TOKEN'))
-
 
     updater.idle()
 
